c8ea184b6e3d8b6f162a5dc5ef292db4e1171224
OmarNizam <eng.omarnizam@gmail.com>
2017-07-04 12:43:35 +0200

    One-to-Many: Models

---
+++ app/models/event.rb
@@ -0,0 +1,14 @@
+class Event < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :location, presence: false
+  validates :price, presence: false
+  validates :capacity, presence: false
+  validates :includes_food, presence:false
+  validates :includes_drinks, presence: false
+  validates :starts_at, presence: false
+  validates :ends_at, presence: false
+  validates :active, presence: false
+end
